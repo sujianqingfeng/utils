6648c35674f77fd9aed417505a61ef150ec9511b
hens <121116111@qq.com>
2022-10-09 23:03:12 +0800

    chore: change pack config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/index.ts'], 
+  entry: ['src/index.ts'], 
   outDir: 'lib',
   treeshake: true,
-  splitting: false,
   clean: true,
   dts: true,
   format: ['cjs', 'esm'],
-  target: 'es5',
-  // sourcemap: true,
-  // minify is iffy
-  // minify: true
+  shims: true
 })
